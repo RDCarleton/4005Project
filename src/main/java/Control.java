32611e7563f4b358779f61fb8de5b47bfdd53ddd
RDCarleton <xevoker007@gmail.com>
2021-03-26 19:55:04 -0400

    changed control to use minutes and seconds, rounded rng time

--- src/main/java/Control.java
+++ src/main/java/Control.java
@@ -10,8 +10,8 @@ public class Control {
 	static Workstation workstation2 = new Workstation(2);
 	static Workstation workstation3 = new Workstation(3);
 	
-	static double sim_time_ms = 0 ;
-	static double sim_time_sec = 0;
+	static double sim_time_sec = 0 ;
+	static double sim_time_min = 0;
 	
 	private static int inspector1_totalIdleTime;
 	private static int inspector2_totalIdleTime;
@@ -34,18 +34,19 @@ public class Control {
 		inspector2_totalIdleTime = 0;
 		
 		//discrete time loop
-	while(sim_time_sec < 500) {
+		// set the simulation time to an 8 hour shift
+	while(sim_time_min < 480) {
 		
-		//Advance time by 1 ms
-		sim_time_ms++;
-		sim_time_sec = sim_time_ms / 1000;
+		//Advance time by 1 sec
+		sim_time_sec++;
+		sim_time_min = sim_time_sec / 60;
 		//Print current system time
 		
-		if (sim_time_ms%10000 ==0) {
+		if (sim_time_min %1 ==0) {
 			System.out.println("---------------");
-			System.out.println("time : " + sim_time_ms); 
+			System.out.println("time : " + sim_time_min);
 			System.out.println("inspector1 status : " + inspector1.getStatus() + ", inspector 2 status: " + inspector2.getStatus());
-			System.out.println("inspector1 total idle time : " + inspector1_totalIdleTime + ", inspector2 total idle time : " + inspector1_totalIdleTime + "\n"); 
+			System.out.println("inspector1 total idle time : " + inspector1_totalIdleTime/60 + ", inspector2 total idle time : " + inspector2_totalIdleTime/60 + "\n");
 		}
 		
 		//Check status of inspector 
@@ -60,8 +61,8 @@ public class Control {
 			//Set inspectors status as busy
 			inspector1.setStatus(1);
 			//Set a time to work on this component 
-			inspector1.setRemainingTime((int) (calculateInspectionTime(1) * 60000));	
-			System.out.println("["+sim_time_ms+"] Inspector 1 is getting a component, inspection time: " + inspector1.getRemainingTime());
+			inspector1.setRemainingTime((int) (calculateInspectionTime(1) * 60));
+			System.out.println("["+ sim_time_min +"] Inspector 1 is getting a component, inspection time: " + inspector1.getRemainingTime()/60);
 		} 
 		
 		if (inspector1.getStatus() == 1) {
@@ -101,15 +102,15 @@ public class Control {
 			//Set inspectors status as busy
 			inspector2.setStatus(1);
 			//Set a time to work on this component 
-			inspector2.setRemainingTime((int) (calculateInspectionTime(randComponent) * 60000));	
-			System.out.println("["+sim_time_ms+"] Inspector 2 is getting a component, inspection time: " + inspector2.getRemainingTime());
+			inspector2.setRemainingTime((int) (calculateInspectionTime(randComponent) * 60));
+			System.out.println("["+ sim_time_min +"] Inspector 2 is getting a component, inspection time: " + inspector2.getRemainingTime()/60);
 		}
 		
 
 		if (inspector2.getStatus() == 1) {
 			//Inspector is inspecting, set remaining time
 			int time_left = inspector2.getRemainingTime();
-			//System.out.println("Inspector 1 is inspecting a component. Remaining time: " +time_left);
+			//System.out.println("Inspector 2 is inspecting a component. Remaining time: " +time_left);
 			if (time_left == 0) {
 				//inspector has finished an inspection
 				inspector2.setRemainingTime(0);
@@ -135,17 +136,17 @@ public class Control {
 		if (workstation1.getStatus() == 0) {
 			if (workstation1.canMake(1)) {
 				workstation1.setStatus(1);
-				workstation1.setProcessingTime((int) (calculateProcessingTime(1) * 60000));
-				System.out.println("["+sim_time_ms+"]["+sim_time_ms+"] WS1 is beginning to process an item at time " + sim_time_ms +", time remaining: " + workstation1.getProcessTime());
-				System.out.println("["+sim_time_ms+"]["+sim_time_ms+"] WS1 buffer size: " + workstation1.getBufferSize(1));
+				workstation1.setProcessingTime((int) (calculateProcessingTime(1) * 60));
+				System.out.println("["+ sim_time_min +"]["+ sim_time_min +"] WS1 is beginning to process an item at time " + sim_time_min +", time remaining: " + workstation1.getProcessTime());
+				System.out.println("["+ sim_time_min +"]["+ sim_time_min +"] WS1 buffer size: " + workstation1.getBufferSize(1));
 			}
 		} else if (workstation1.getStatus() == 1) {
 			int Ptime = workstation1.getProcessTime();
 			if (Ptime > 0) {
 				workstation1.setProcessingTime(Ptime-1);
 			} else if (Ptime == 0) {
-				System.out.println("["+sim_time_ms+"]["+sim_time_ms+"] WS1 has finished creating a product at time " + sim_time_ms);
-				System.out.println("["+sim_time_ms+"] WS1 buffer size (before finishing): " + workstation1.getBufferSize(1));
+				System.out.println("["+ sim_time_min +"]["+ sim_time_sec +"] WS1 has finished creating a product at time " + sim_time_min);
+				System.out.println("["+ sim_time_min +"] WS1 buffer size (before finishing): " + workstation1.getBufferSize(1));
 				workstation1.finishProduct(1); //this method sets status back to 0, and removes comps from buffer
 				
 			}
@@ -153,16 +154,16 @@ public class Control {
 		
 		if (workstation2.getStatus() == 0 && workstation2.canMake(2)) {
 			workstation2.setStatus(1);
-			workstation2.setProcessingTime((int) (calculateProcessingTime(2) * 60000));
-			System.out.println("["+sim_time_ms+"] WS2 is beginning to process an item at time " + sim_time_ms +", time remaining: " + workstation2.getProcessTime());
-			System.out.println("["+sim_time_ms+"] WS2 buffer1 size: "+ workstation2.getBufferSize(1) +", WS2 buffer2 size: " + workstation2.getBufferSize(2));
+			workstation2.setProcessingTime((int) (calculateProcessingTime(2) * 60));
+			System.out.println("["+ sim_time_min +"] WS2 is beginning to process an item at time " + sim_time_min +", time remaining: " + workstation2.getProcessTime());
+			System.out.println("["+ sim_time_min +"] WS2 buffer1 size: "+ workstation2.getBufferSize(1) +", WS2 buffer2 size: " + workstation2.getBufferSize(2));
 		} else if (workstation2.getStatus() == 1) {
 			int Ptime = workstation2.getProcessTime();
 			if (Ptime > 0) {
 				workstation2.setProcessingTime(Ptime-1);
 			} else if (Ptime == 0) {
-				System.out.println("["+sim_time_ms+"] WS2 has finished creating a product at time " + sim_time_ms);
-				System.out.println("["+sim_time_ms+"] WS2 buffer1 size (before finishing): " + workstation2.getBufferSize(1) + ", WS2 buffer2 size:" + workstation2.getBufferSize(2));
+				System.out.println("["+ sim_time_min +"] WS2 has finished creating a product at time " + sim_time_min);
+				System.out.println("["+ sim_time_min +"] WS2 buffer1 size (before finishing): " + workstation2.getBufferSize(1) + ", WS2 buffer2 size:" + workstation2.getBufferSize(2));
 				workstation2.finishProduct(2); //this method sets status back to 0, and removes comps from buffer
 				
 			}
@@ -171,16 +172,16 @@ public class Control {
 		
 		if (workstation3.getStatus() == 0 && workstation3.canMake(3)) {
 			workstation3.setStatus(1);
-			workstation3.setProcessingTime((int) (calculateProcessingTime(3) * 60000));
-			System.out.println("["+sim_time_ms+"] WS3 is beginning to process an item at time " + sim_time_ms +", time remaining: " + workstation3.getProcessTime());
-			System.out.println("["+sim_time_ms+"] WS3 buffer1 size: "+ workstation3.getBufferSize(1) +", WS3 buffer3 size: " + workstation3.getBufferSize(3));
+			workstation3.setProcessingTime((int) (calculateProcessingTime(3) * 60));
+			System.out.println("["+ sim_time_min +"] WS3 is beginning to process an item at time " + sim_time_min +", time remaining: " + workstation3.getProcessTime());
+			System.out.println("["+ sim_time_min +"] WS3 buffer1 size: "+ workstation3.getBufferSize(1) +", WS3 buffer3 size: " + workstation3.getBufferSize(3));
 		} else if (workstation3.getStatus() == 1) {
 			int Ptime = workstation3.getProcessTime();
 			if (Ptime > 0) {
 				workstation3.setProcessingTime(Ptime-1);
 			} else if (Ptime == 0) {
-				System.out.println("["+sim_time_ms+"] WS3 has finished creating a product at time " + sim_time_ms);
-				System.out.println("["+sim_time_ms+"] WS3 buffer1 size (before finishing): " + workstation3.getBufferSize(1) + ", WS3 buffer3 size:" + workstation3.getBufferSize(3));
+				System.out.println("["+ sim_time_min +"] WS3 has finished creating a product at time " + sim_time_min);
+				System.out.println("["+ sim_time_min +"] WS3 buffer1 size (before finishing): " + workstation3.getBufferSize(1) + ", WS3 buffer3 size:" + workstation3.getBufferSize(3));
 				workstation3.finishProduct(3); //this method sets status back to 0, and removes comps from buffer
 				
 			}
@@ -204,24 +205,28 @@ public class Control {
 	        double randomD;
 	        randomNum = RandomGen.nextInt();
 	        // Calc within bound by dividing by max value
-	        randomD = randomNum/Integer.MAX_VALUE;
+	        randomD = Math.abs(randomNum/Integer.MAX_VALUE);
 	        
 	        switch (inspector) {
 	        	case 1:	
 	        		 	// CDF calculation for inspection times
 	        			x = (-1/ins1Lambda)*Math.log(1-randomD);
-	        			time = ins1Lambda*Math.exp(-ins1Lambda*x);
+	        			time = (-1/ins1Lambda)*Math.log(1-randomD);
+	        			time = Math.round(time*100.0)/100.0;
+
 	        			break;
 	        	case 2:	
         		 	// CDF calculation for inspection times
         			x = (-1/ins2Lambda)*Math.log(1-randomD);
-        			time = ins2Lambda*Math.exp(-ins2Lambda*x);
-        			break;
+        			time = (-1/ins2Lambda)*Math.log(1-randomD);
+					time = Math.round(time*100.0)/100.0;
+					break;
 	        	case 3:	
         		 	// CDF calculation for inspection times
         			x = (-1/ins3Lambda)*Math.log(1-randomD);
-        			time = ins3Lambda*Math.exp(-ins3Lambda*x);
-        			break;
+        			time = (-1/ins3Lambda)*Math.log(1-randomD);
+					time = Math.round(time*100.0)/100.0;
+					break;
         		default : x = 0; time = 0;
 	        }
 	        	        
@@ -238,26 +243,29 @@ public class Control {
 	        double randomD;
 	        randomNum = RandomGen.nextInt();
 	        // Calc within bound by dividing by max value
-	        randomD = randomNum/Integer.MAX_VALUE;
+	        randomD = Math.abs(randomNum/Integer.MAX_VALUE);
 	        
 	        switch (workstation) {
 	        	case 1:	
 	        		 	// CDF calculation for inspection times
 	        			x = (-1/ws1Lambda)*Math.log(1-randomD);
-	        			time = ws1Lambda*Math.exp(-ws1Lambda*x);
+	        			time = (-1/ws1Lambda)*Math.log(1-randomD);
+						time = Math.round(time*100.0)/100.0;
 
-	        			System.out.println("WS1: x = " + x+ ", time = " + time);
+						System.out.println("WS1: x = " + x+ ", time = " + time);
 	        			break;
 	        	case 2:	
         		 	// CDF calculation for inspection times
         			x = (-1/ws2Lambda)*Math.log(1-randomD);
-        			time = ws2Lambda*Math.exp(-ws2Lambda*x);
-        			break;
+        			time = (-1/ws2Lambda)*Math.log(1-randomD);
+					time = Math.round(time*100.0)/100.0;
+					break;
 	        	case 3:	
         		 	// CDF calculation for inspection times
         			x = (-1/ws3Lambda)*Math.log(1-randomD);
-        			time = ws3Lambda*Math.exp(-ws3Lambda*x);
-        			break;
+        			time = (-1/ws3Lambda)*Math.log(1-randomD);
+					time = Math.round(time*100.0)/100.0;
+					break;
         		default : time = 0;
 	        }
 	        	        
@@ -280,22 +288,22 @@ public class Control {
 			//if buffers are full, inspector is blocked
 				if (bufferSize1 == 0) {
 					workstation1.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS1, buffer size: " + workstation1.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS1, buffer size: " + workstation1.getBufferSize(1));
 				} else if (bufferSize2 == 0) {
 					workstation2.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(1));
 				} else if (bufferSize3 == 0) {
 					workstation3.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(1));
 				} else if (bufferSize1 == 1) {
 					workstation1.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS1, buffer size: " + workstation1.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS1, buffer size: " + workstation1.getBufferSize(1));
 				} else if (bufferSize2 == 1) {
 					workstation2.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(1));
 				} else if (bufferSize3 == 1) {
 					workstation3.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 1 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(1));
+					System.out.println("["+ sim_time_min +"] Component 1 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(1));
 				} else {
 					//System.out.println("["+sim_time_ms+"] Inspector 1 is blocked!");
 					//set status to block
@@ -313,7 +321,7 @@ public class Control {
 			//if buffers are full, inspector is blocked
 				if (bufferSize < 2 ) {
 					workstation2.addToBuffer(comp);
-					System.out.println("["+sim_time_ms+"] Component 2 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(2));
+					System.out.println("["+ sim_time_min +"] Component 2 has been inspected, sent to WS2, buffer size: " + workstation2.getBufferSize(2));
 				} else {
 					//System.out.println("["+sim_time_ms+"] Inspector 2 is blocked with component 2!");
 					//set status to block
@@ -328,7 +336,7 @@ public class Control {
 			
 			if (bufferSize < 2 ) {
 				workstation3.addToBuffer(comp);
-				System.out.println("["+sim_time_ms+"] Component 3 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(3));
+				System.out.println("["+ sim_time_min +"] Component 3 has been inspected, sent to WS3, buffer size: " + workstation3.getBufferSize(3));
 			} else {
 				//System.out.println("["+sim_time_ms+"] Inspector 2 is blocked with component 3!");
 				//set status to block
